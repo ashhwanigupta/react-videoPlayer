c0f3c53e0dd4f5e0a532c92d75d9d563c633e46a
Ashwani Gupta <Ashhwanigupta1@gmail.com>
2020-05-14 12:30:55 +0530

    Updates

---
+++ precache-manifest.979d9cbd77920e5f820f50c1ac2a8ffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d016f0ce263d7c41bfc6ebc854ca59",
+    "url": "/react-videoPlayer/index.html"
+  },
+  {
+    "revision": "eb68d8d7ba46c3493e63",
+    "url": "/react-videoPlayer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bcb33e34a6c5689f234d",
+    "url": "/react-videoPlayer/static/js/2.4ad7200a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-videoPlayer/static/js/2.4ad7200a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb68d8d7ba46c3493e63",
+    "url": "/react-videoPlayer/static/js/main.2172d4ab.chunk.js"
+  },
+  {
+    "revision": "f7b3933baf24cf4c248e",
+    "url": "/react-videoPlayer/static/js/runtime-main.f1622e19.js"
+  }
+]);
